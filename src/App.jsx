8aea5fb98e5136350e47112eba467d6dda6d199e
sku11ster <murtazaahmed022@gmail.com>
2024-04-19 10:11:07 +0500

    Adding Hero Animation

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div id="main">
+          <div className='bg-red-500 absolute h-[50vh] w-full top-0 opacity-25'></div>
+          <div className='bg-black min-h-screen w-full flex flex-row justify-center items-center text-center font-sans text-8xl font-bold text-white'>This is my TEXT</div>
+          <div className='bg-blue-500 absolute h-[50vh] w-full bottom-0 opacity-25'></div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
